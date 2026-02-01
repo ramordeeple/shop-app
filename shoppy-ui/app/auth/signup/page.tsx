c62de753cf4ef60cc82266a2b452fd8c99c31a08
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-02-01 18:08:51 +0300

    Add signup page

---
+++ shoppy-ui/app/auth/signup/page.tsx
@@ -0,0 +1,15 @@
+import { Stack, TextField, Button } from '@mui/material';
+import Link from 'next/link';
+
+export default function Signup() {
+    return (
+        <Stack spacing={2} className="w-full max-w-xs">
+            <TextField label="Email" variant="outlined" type="email" />
+            <TextField label="Password" variant="outlined" type="password" />
+            <Button variant="contained">Sign up</Button>
+            <Link href="/auth/login" className="self-center">
+                Login
+            </Link>
+        </Stack>
+    );
+}
