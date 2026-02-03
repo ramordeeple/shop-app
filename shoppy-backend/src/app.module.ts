bb6566b5c3c6c145e164f8e0edc063251516d1fb
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-02-03 20:44:20 +0300

    Add config module with its service for Logger

--- shoppy-backend/src/app.module.ts
+++ shoppy-backend/src/app.module.ts
@@ -1,32 +1,36 @@
-import {Module} from '@nestjs/common';
-import {UsersModule} from './users/users.module';
-import {ConfigModule, ConfigService} from '@nestjs/config';
-import {LoggerModule} from 'nestjs-pino';
-import {pinoHttp} from 'pino-http';
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-    imports: [
-        LoggerModule.forRootAsync({
-            useFactory: (configService: ConfigService) => {
-                const isProduction = configService.get('NODE_ENV') === 'production'
+	imports: [
+		LoggerModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: (configService: ConfigService) => {
+				const isProduction =
+					configService.get('NODE_ENV') === 'production';
 
-                return {
-                    pinoHttp: {
-                        transport: isProduction ? undefined : {
-                            target: 'pino-pretty',
-                            options: {
-                                singleLine: true,
-                            },
-                        },
-                        level: isProduction ? 'info' : 'debug'
-                    },
-                };
-            },
-        }),
-        ConfigModule.forRoot(),
-        UsersModule],
-    controllers: [],
-    providers: [],
+				return {
+					pinoHttp: {
+						transport: isProduction
+							? undefined
+							: {
+									target: 'pino-pretty',
+									options: {
+										singleLine: true,
+									},
+								},
+						level: isProduction ? 'info' : 'debug',
+					},
+				};
+			},
+			inject: [ConfigService],
+		}),
+		ConfigModule.forRoot(),
+		UsersModule,
+	],
+	controllers: [],
+	providers: [],
 })
-export class AppModule {
-}
+export class AppModule {}
