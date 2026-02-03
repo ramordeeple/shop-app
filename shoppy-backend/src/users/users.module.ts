11a2b76c437e2ed7d6a40efaddcbe1311ee58275
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-02-03 20:13:22 +0300

    Fix module imports

--- shoppy-backend/src/users/users.module.ts
+++ shoppy-backend/src/users/users.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import {ConfigModule} from '@nestjs/config';
 
 @Module({
-	imports: [ConfigModule.forRoot(), UsersModule],
-	controllers: [],
+	controllers: [UsersController],
 	providers: [UsersService],
 })
 export class UsersModule {}
