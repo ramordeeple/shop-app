254a22ac680fad314b6a835b2d275b37b1f203ab
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-02-03 20:38:44 +0300

    Add console log with created user

--- shoppy-backend/src/users/users.service.ts
+++ shoppy-backend/src/users/users.service.ts
@@ -3,5 +3,7 @@ import { CreateUserRequest } from './dto/create-user.request';
 
 @Injectable()
 export class UsersService {
-	createUser(data: CreateUserRequest) {}
+	createUser(data: CreateUserRequest) {
+		console.log(data);
+	}
 }
